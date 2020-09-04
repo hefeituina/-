b7f44e3184b7fda76b482706cc470ad33141f8c3
hefeituina <70738958+hefeituina@users.noreply.github.com>
2020-09-04 14:54:00 +0800

    ftppp2
    ftppp2

--- HelloWork.java
+++ HelloWork.java
@@ -15,12 +15,6 @@ import java.util.Map;
 public class HelloWork extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //super.doGet(req, resp);
-        Map<String,Object> map=new HashMap<String,Object>();
-        map.put("name","tx");
-        String json=JSONObject.toJSONString(map);
-        resp.getWriter().print(json);
-        resp.getWriter().flush();
-        resp.getWriter().close();
+        super.doGet(req, resp);
     }
 }
